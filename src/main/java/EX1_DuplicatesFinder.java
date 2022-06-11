e6eec84b6ecb2c36707361e36fe98eace226d36e
RashmiRanjanAcharya <RashmiRanjanAcharya@users.noreply.github.com>
2022-06-11 20:14:10 +0530

    EX1_DuplicatesFinder Implementation
    EX1_DuplicatesFinder Implementation Added By Rashmi Ranjan Acharya

--- src/main/java/EX1_DuplicatesFinder.java
+++ src/main/java/EX1_DuplicatesFinder.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class EX1_DuplicatesFinder {
   //Instructions:
@@ -7,6 +11,15 @@ public class EX1_DuplicatesFinder {
   //  is provided, but your code should perform for an array of any number of integers.
   public static List<Integer> findDuplicates(int[] inputArray) {
     //YOUR CODE HERE
-    return null;
+	  List<Integer> duplicatedNumbersList = Arrays.stream(inputArray).boxed()
+			  									  .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+			  									  .entrySet().stream().filter(i->i.getValue()>1).map(Map.Entry::getKey)
+			  									  .collect(Collectors.toList());
+	  return duplicatedNumbersList;
+  }
+  
+  public static void main(String[] args) {
+	  List<Integer> duplicatedNumbersList = findDuplicates(new int[]{4,6,1,4,2,1,6,5,3,6});
+	  System.out.println(duplicatedNumbersList.);
   }
 }
