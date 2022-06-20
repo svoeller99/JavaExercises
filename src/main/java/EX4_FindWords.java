630f2c284a493e21aa44c3d102737c0f6876d3ea
RathiRobin <robin.cest@gmail.com>
2022-06-20 17:03:13 +0530

    C-1-robinkumar/22m06

--- src/main/java/EX4_FindWords.java
+++ src/main/java/EX4_FindWords.java
@@ -23,8 +23,50 @@ import java.util.List;
 //
 //    Output: ["eat","oath"]
 public class EX4_FindWords {
-  public static List<String> findWords(String[][] board, List<String> wordsToFind) {
-    // YOUR CODE HERE
-    return null;
-  }
+	public static List<String> findWords(String[][] board, List<String> wordsToFind) {
+		// YOUR CODE HERE
+		return null;
+	}
+
+	static final String wordsToFind[] = { "oath", "pea", "eat", "rain" };
+	static final int n = wordsToFind.length;
+	static final int M = 3, N = 3;
+
+	static boolean isWordFound(String str) {
+		for (int i = 0; i < n; i++)
+			if (str.equals(wordsToFind[i]))
+				return true;
+		return false;
+	}
+
+	static void findWordsUtil(char board[][], boolean wordsToFind[][], int i, int j, String str) {
+        wordsToFind[i][j] = true;
+		str = str + board[i][j];
+
+		if (isWordFound(str))
+			System.out.println(str);
+
+		for (int row = i - 1; row <= i + 1 && row < M; row++)
+			for (int col = j - 1; col <= j + 1 && col < N; col++)
+				if (row >= 0 && col >= 0 && !wordsToFind[row][col])
+					findWordsUtil(board, wordsToFind, row, col, str);
+
+		str = "" + str.charAt(str.length() - 1);
+		wordsToFind[i][j] = false;
+	}
+
+	static void findWords(char board[][]) {
+		boolean wordsToFind[][] = new boolean[M][N];
+		String str = "";
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < N; j++)
+				findWordsUtil(board, wordsToFind, i, j, str);
+	}
+
+	public static void main(String args[]) {
+		char[][] board = { { 'o', 'a', 'a', 'n' }, { 'e', 't', 'a', 'e' }, { 'i', 'h', 'k', 'r' },
+				{ 'i', 'f', 'l', 'v' } };
+		System.out.println("2D Board-words of dictionary are present");
+		findWords(board);
+	}
 }
